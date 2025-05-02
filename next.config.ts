3e2581792f39ad913524f490cb54ddffa0f5ca29
tro3373 <tro3373@gmail.com>
2025-05-03 06:08:39 +0900

    feat: Add rewrites/redirects route

--- next.config.ts
+++ next.config.ts
@@ -4,20 +4,20 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/satoshi.blogs.com/:path*",
-        destination: "https://satoshi.blogs.com/:path*",
+        source: "/rewrites/:path*",
+        destination: "https://:path*",
       },
     ];
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/satoshi.blogs.com/:file*",
-  //       destination: "https://satoshi.blogs.com/:file*",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: "/redirects/:path*",
+        destination: "https://:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
